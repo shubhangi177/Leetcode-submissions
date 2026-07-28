679b628a34488ca342eb782a0ebb48eb8e75d2c4
shubhangi177 <shubhangii7777@gmail.com>
2026-07-28 21:34:09 +0530

    Time: 0 ms (100%), Space: 42.7 MB (94.79%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.java
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -16,20 +16,20 @@ class Solution {
     public boolean backspaceCompare(String s, String t) {
         int i = s.length() - 1;
         int j = t.length() - 1;
-        while(i>=0 || j>=0){
+        while(i >= 0 || j >= 0){
+
             i = getValidIndex(s, i);
             j = getValidIndex(t, j);
 
-            // If one string has no valid character left but the other does,
-            // they can't be equal.
-            if((i>=0)!=(j>=0)) return false;
+            if(i < 0 && j < 0)
+                break;
 
-            // If both have valid characters but they are different,
-            // they can't be equal.
-            else if(i>=0 && s.charAt(i)!=t.charAt(j)) return false;
+            if(i < 0 || j < 0)
+                return false;
 
-            // Otherwise, we've matched this pair of characters,
-            // so move left and repeat.
+            if(s.charAt(i) != t.charAt(j))
+                return false;
+
             i--;
             j--;
         }
