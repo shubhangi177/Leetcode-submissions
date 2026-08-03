1960da3e6bcc3b547b56af68cce255dd1c97b99f
shubhangi177 <shubhangii7777@gmail.com>
2026-08-03 11:51:07 +0530

    Time: 5 ms (29.69%), Space: 81.7 MB (95.01%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
@@ -14,6 +14,7 @@
  * }
  */
 class Solution {
+    /* Solution 1:BFS 
     public int minDepth(TreeNode root) {
         if(root == null) return 0;
         Queue<TreeNode> queue = new LinkedList<>();
@@ -36,5 +37,23 @@ class Solution {
             level++;
         }
         return level;
+    }*/
+    
+    /** Solution 2: DFS 
+    * Key point: 
+    * if a node only has one child -> MUST return the depth of the side with child, i.e. MAX(left, right) + 1
+    * if a node has two children on both side -> return min depth of two sides, i.e. MIN(left, right) + 1
+    * */
+    public int minDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+        if(left == 0|| right == 0){
+            return Math.max(left, right)+1;
+        }else{
+            return Math.min(left, right)+1;
+        }
     }
 }
